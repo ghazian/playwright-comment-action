df3a81b7b067d04cfb26cdb4e693c7f6a6c0d577
Ghazian Azfar <azfar.ghazian@gmail.com>
2023-08-23 23:00:43 +0100

    add json file path

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { context, getOctokit } from "@actions/github";
 
 export async function run() {
 	const token = getInput("gh-token");
-	const prComment = getInput("prcomment");
+	const jsonfile = getInput("jsonfile");
 
 	const octokit = getOctokit(token);
 	const pullRequest = context.payload.pull_request;
@@ -16,7 +16,7 @@ export async function run() {
 		await octokit.rest.issues.createComment({
 			...context.repo,
 			issue_number: pullRequest.number,
-			body: prComment,
+			body: jsonfile,
 		});
 	} catch (error) {
 		setFailed((error as Error)?.message ?? "Unknown error");
